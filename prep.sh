ad3893c133736ac623d8c56b3362235f213285eb
Anthony Green <green@moxielogic.com>
2025-01-19 04:08:15 -0500

    Patch in the right dir

--- prep.sh
+++ prep.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -x
+
 # The clamps build only works if ironclad is built first.  If ironclad
 # is built as part of the clamps build, there will be an error.
 
@@ -7,5 +9,5 @@ sbcl --eval "(asdf:load-system :ironclad)" --eval "(quit)"
 
 # Apply this patch....
 
-ls -l
-patch -p1 < ../420a19ec737d2641bf6db458da70ac03efaba557.patch
+(cd clamps-*;
+ patch -p1 < ../420a19ec737d2641bf6db458da70ac03efaba557.patch)
